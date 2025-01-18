7f426273bc342e0de2859e523cd3bba2054c9ffb
Marcos Raimondi <marcosraimondi1@hotmail.com>
2025-01-18 10:43:29 -0300

    circles orbiting each other

--- src/main.cpp
+++ src/main.cpp
@@ -12,13 +12,13 @@ int main() {
   sf::ContextSettings settings;
   settings.antialiasingLevel = 8;
   auto window =
-      sf::RenderWindow(sf::VideoMode(sf::VideoMode::getDesktopMode()),
-                       "Fourier Visualization", sf::Style::Default, settings);
+      sf::RenderWindow(sf::VideoMode({800, 800}), "Fourier Visualization",
+                       sf::Style::Default, settings);
   window.setFramerateLimit(144);
   ImGui::SFML::Init(window);
   sf::Clock clock;
 
-  int nWaves = 10;
+  int nWaves = 1;
   int nextSize = nWaves;
   float freq = 1;
   float time = 0;
@@ -43,13 +43,28 @@ int main() {
     n += 2;
   }
 
+  sf::VertexArray lines =
+      sf::VertexArray(sf::PrimitiveType::LineStrip, series.size() + 1);
+  for (int i = 0; i < nWaves; i++) {
+    lines[i].position = series[i].getPosition();
+  }
+
+  sf::CircleShape dot;
+  dot.setRadius(2);
+  dot.setOrigin({dot.getRadius(), dot.getRadius()});
+
   while (window.isOpen()) {
+
+    // INPUT ----------------
+
     for (auto event = sf::Event(); window.pollEvent(event);) {
       if (event.type == sf::Event::Closed) {
         window.close();
       }
       ImGui::SFML::ProcessEvent(event);
     }
+
+    // UPDATE ---------------
 
     sf::Time elapsed = clock.restart();
     ImGui::SFML::Update(window, elapsed);
@@ -67,11 +82,12 @@ int main() {
       for (auto &s : series) {
         s.setRadius(amplitude / n);
         s.setOrigin({s.getRadius(), s.getRadius()});
-        s.setPosition(initialPosition);
         s.setFillColor(sf::Color::Transparent);
         s.setOutlineThickness(1);
         n += 2;
       }
+
+      lines = sf::VertexArray(sf::PrimitiveType::LineStrip, series.size() + 1);
     }
 
     time += elapsed.asSeconds();
@@ -79,15 +95,19 @@ int main() {
     n = 1;
     float x = initialPosition.x, y = initialPosition.y;
 
+    int m = 0;
     for (auto &s : series) {
       x += s.getRadius() * std::cos(n * time * freq);
       y += s.getRadius() * std::sin(n * time * freq);
 
       sf::Vector2f newPos = {x, y};
 
-      s.setPosition(newPos);
+      if (m + 1 < series.size())
+        series[m + 1].setPosition(newPos);
+      m++;
       n += 2;
     }
+    series[0].setPosition(initialPosition);
 
     for (int i = wave.getVertexCount() - 1; i > 0; i--) {
       wave[i].position.y = wave[i - 1].position.y;
@@ -96,8 +116,17 @@ int main() {
     wave[0].position.x = initialPosition.x + 200;
     wave[0].position.y = y;
 
-    line[0].position = series.back().getPosition();
+    line[0].position = {x, y};
     line[1].position = wave[0].position;
+
+    dot.setPosition({x, y});
+
+    for (int i = 0; i < nWaves; i++) {
+      lines[i].position = series[i].getPosition();
+    }
+    lines[nWaves].position = {x, y};
+
+    // DRAW -------------------------
 
     window.clear();
     ImGui::SFML::Render(window);
@@ -107,6 +136,9 @@ int main() {
 
     window.draw(wave);
     window.draw(line);
+    window.draw(lines);
+
+    window.draw(dot);
 
     window.display();
   }
